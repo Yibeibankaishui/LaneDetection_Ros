7bcb9ec60e2795ae24827cbecbac3280788930e2
byc <yibeibankaishui@163.com>
2021-12-15 07:47:22 +0000

    add camera and fix a bug

--- src/sign_detection/sign_detection.py
+++ src/sign_detection/sign_detection.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import cv2
 import numpy as np
 import math
@@ -5,7 +6,7 @@ import matplotlib.pyplot as plt
 
 resize_ratio = 1
 
-search_N = 7    # 搜索邻域半径
+search_N = 7    
 
 show = False
 
@@ -62,7 +63,8 @@ def sign_detector(input_img, if_show):
 
     img_blue = cv2.inRange(hsv_img, low_hsv, high_hsv)
 
-    contours, hierarchy = cv2.findContours(img_blue,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)#contours为轮廓集，可以计算轮廓的长度、面积等
+    ret, contours, hierarchy = cv2.findContours(img_blue,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)#contours为轮廓集，可以计算轮廓的长度、面积等
+
     if (contours == []):
         # 没有轮廓返回
         print("no CONTOURS!")
@@ -107,47 +109,9 @@ def sign_detector(input_img, if_show):
         return 0
 
 
-
-# # 如果图片中有标志，则进行判断    
-# def sign_read(input_img):            
-
-#     img = cv2.resize(input_img, (int(input_img.shape[1]*resize_ratio), int(input_img.shape[0]*resize_ratio)), interpolation = cv2.INTER_AREA)
-
-#     hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-#     # 180，255，255
-#     low_hsv = np.array([100, 88, 0])
-#     high_hsv = np.array([130, 255, 255])
-
-#     img_blue = cv2.inRange(hsv_img, low_hsv, high_hsv)
-
-#     contours, hierarchy = cv2.findContours(img_blue,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)#contours为轮廓集，可以计算轮廓的长度、面积等
-
-#     # 找椭圆
-#     for cnt in contours:
-#         if len(cnt)>50:
-#             # 围合区域的面积
-#             S1 = cv2.contourArea(cnt)
-#             # bounding box
-#             ell = cv2.fitEllipse(cnt)
-#             # 拟合椭圆的面积
-#             S2 = (0.25) * math.pi * ell[1][0] * ell[1][1]     # 外接最小矩形 质心（圆心），边长，角度
-#             if (S1/S2) > 0.85 :#面积比例，可以更改，根据数据集。。。
-#                 (bb_x, bb_y, bb_w, bb_h) = cv2.boundingRect(cnt)
-
-#     img_mask = np.zeros_like(img_blue)
-#     img_mask[bb_y:bb_y+bb_h,bb_x:bb_x+bb_w] = 255
-#     img_res = cv2.bitwise_and(img_mask,img_blue)
-#     # 统计水平方向的像素个数
-#     sum_for_x = np.sum(img_res, 0)
-
-#     result = find_min(sum_for_x)    # -1 left;  1 right
-
-#     return result
-
 if __name__ == "__main__":
     #加载图像img
-    img = cv2.imread('./images/signs/signs/IMG_3134 Medium.jpeg',cv2.IMREAD_COLOR)
+    img = cv2.imread('/home/ubuntu/imges_for_test/signs/IMG_3134 Medium.jpeg',cv2.IMREAD_COLOR)
     # sign_read(img)
     sign_detector(img,0)
    
